701c694ad0048a35bcf92f7c38c9e473816f0f7c
umitduran <umitduran35@gmail.com>
2021-10-06 22:08:51 +0300

    fixing app spec file

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -7,28 +7,31 @@ import App from './App';
 describe('components > App', () => {
   const mockStore = configureStore([]);
   const store = mockStore({
-    userInfo: {
+    count: {
+      value: 6,
+    },
+    random: {
       isLoading: false,
       hasError: false,
       isFulfilled: false,
     },
   });
 
-  /*  it('renders without crashing', () => {
-    /!**
+  it('renders without crashing', () => {
+    /**
      * `asFragment`:
      * @see https://testing-library.com/docs/react-testing-library/api#asfragment
      * `wrapper`
      * @see https://testing-library.com/docs/react-testing-library/api#wrapper
-     *!/
+     */
     const {asFragment} = render(<App />, {
       wrapper: ({children}) => <Provider store={store}>{children}</Provider>,
     });
 
-    /!**
+    /**
      * Basic snapshot test to make sure, that rendered component
      * matches expected footprint.
-     *!/
+     */
     expect(asFragment()).toMatchSnapshot();
-  }); */
+  });
 });
